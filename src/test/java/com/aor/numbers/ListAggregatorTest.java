edffa43cb9bc98600a9dcdf65a80361cdbef9177
Marta <up202207879@up.pt>
2023-11-26 10:32:14 +0000

    Ponto-7

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -3,59 +3,62 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListAggregatorTest {
     private List<Integer> list;
-    private static class DeduplicatorStub implements GenericListDeduplicator {
-        @Override
-        public List<Integer> deduplicate(List<Integer> list) {
-            return Arrays.asList(1, 2, 4); // Stub always returns this result
-        }
-    }
 
     @BeforeEach
     public void setup() {
-        list = Arrays.asList(1, 2, 4, 2);
+        list = Arrays.asList(1, 4, 2);
     }
 
     @Test
     public void sum() {
-        ListAggregator aggregator = new ListAggregator(new DeduplicatorStub());
+        ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
-        Assertions.assertEquals(9, sum); // 1 + 2 + 4 + 2 = 9
+        Assertions.assertEquals(7, sum); // 1 + 2 + 4 + 2 = 9
     }
 
     @Test
     public void max() {
 
 
-        int max = Integer.MIN_VALUE;
-
-        for (Integer number : list)
-            if (number > max)
-                max = number;
+        ListAggregator aggregator = new ListAggregator();
+        int max = aggregator.max(list);
 
         Assertions.assertEquals(4, max);
     }
 
     @Test
     public void min() {
-        ListAggregator aggregator = new ListAggregator(new DeduplicatorStub());
+        ListAggregator aggregator = new ListAggregator();
         int min = aggregator.min(list);
         Assertions.assertEquals(1, min); // Valor mínimo na lista
     }
 
     @Test
     public void distinct() {
-        ListAggregator aggregator = new ListAggregator(new DeduplicatorStub());
-        int distinct = aggregator.distinct(list);
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
+
+        // Set up the stub for deduplicate
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4));
+
+        // Create a ListAggregator instance and inject the mock deduplicator
+        ListAggregator aggregator = new ListAggregator();
+
+
+        // Perform the test
+        int distinct = aggregator.distinct(Arrays.asList(1, 4, 2));
+
+        // Assert the result
         Assertions.assertEquals(3, distinct);
     }
     public void max_bug_7263() {
-        ListAggregator aggregator = new ListAggregator(new DeduplicatorStub());
+        ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(Arrays.asList(-1, -4, -5));
         Assertions.assertEquals(-1, max); // Assuming the fixed max method returns -1 for the given list
     }
--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -3,16 +3,31 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.*;
 
-public class ListDeduplicatorTest implements GenericListDeduplicator {
+public class ListDeduplicatorTest {
     private List<Integer> list;
     private List<Integer> expected;
-    private ListDeduplicator deduplicator;
+    private GenericListDeduplicator deduplicator;
 
+    @BeforeEach
+    public void setup() {
+        list = Arrays.asList(1, 2, 3, 2, 4);
+        expected = Arrays.asList(1, 2, 3, 4);
+        deduplicator = Mockito.mock(GenericListDeduplicator.class);
 
-    public List<Integer> deduplicate(List<Integer> list) {
-        return Arrays.asList(1, 2, 4); // O stub sempre retorna este resultado
+        // Set up the stub
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(expected);
+    }
+
+    @Test
+    public void deduplicate() {
+        // Use the deduplicator stub
+        List<Integer> result = deduplicator.deduplicate(list);
+
+        // Perform the test
+        Assertions.assertEquals(expected, result);
     }
 }
